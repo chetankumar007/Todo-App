c628b4135cc0617538f0fbe981d3f36c80ec38aa
chetankumar007 <chetannooli94@gmail.com>
2024-02-15 19:09:34 +0530

    Commit firest

---
+++ src/data/data.js
@@ -0,0 +1,42 @@
+import amazon from "../assets/projects/amazon_logo.png";
+import netflix from "../assets/projects/netflix.png";
+import todo from "../assets/projects/todo.png";
+import blog from "../assets/projects/blog.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "Amazon Clone",
+    image: amazon,
+    github: "https://github.com/chetankumar007/AmazonClone",
+    live: "",
+  },
+  {
+    id: 2,
+    name: "Netflix Clone",
+    image: netflix,
+    github: "",
+    live: "",
+  },
+  {
+    id: 3,
+    name: "Blog List using MERN Stack",
+    image: blog,
+    github: "",
+    live: "",
+  },
+  {
+    id: 4,
+    name: "Todo App",
+    image: todo,
+    github: "",
+    live: "",
+  },
+  {
+    id: 5,
+    name: "Weather App",
+    image: todo,
+    github: "https://github.com/chetankumar007/Weather-app",
+    live: "",
+  },
+];
